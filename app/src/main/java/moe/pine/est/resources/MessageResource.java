8637f77f8b6f2a852fefa4514737b78a27b50c2d
Pine Mizune <pinemz@gmail.com>
2019-04-18 14:52:52 +0900

    chore: add debug log

--- app/src/main/java/moe/pine/est/resources/MessageResource.java
+++ app/src/main/java/moe/pine/est/resources/MessageResource.java
@@ -3,10 +3,10 @@ package moe.pine.est.resources;
 import lombok.extern.jbosslog.JBossLog;
 import moe.pine.est.filter.FilterGroup;
 import moe.pine.est.mailgun.Mailgun;
-import moe.pine.est.mailgun.Message;
 
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
@@ -21,10 +21,18 @@ public class MessageResource {
     private FilterGroup filterGroup;
 
     @POST
-    public String receive() {
-        log.infov("{0}", filterGroup);
+    public String receive(
+        @FormParam("recipient") final String recipient,
+        @FormParam("sender") final String sender,
+        @FormParam("from") final String from,
+        @FormParam("subject") final String subject
+    ) {
+        log.infov(
+            "Message received :: recipient={0}, sender={1}, from={2}, subject={3}",
+            recipient, sender, from, subject);
 
-        filterGroup.doFilter(Message.builder().build());
+        // filterGroup.doFilter(Message.builder().build());
+
         return "OK";
     }
 }
