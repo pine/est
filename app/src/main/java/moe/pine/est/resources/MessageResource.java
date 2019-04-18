21c848614f3c74986cf734573b0d22aa20e7edfa
Pine Mizune <pinemz@gmail.com>
2019-04-18 15:48:30 +0900

    chore: debug

--- app/src/main/java/moe/pine/est/resources/MessageResource.java
+++ app/src/main/java/moe/pine/est/resources/MessageResource.java
@@ -25,11 +25,13 @@ public class MessageResource {
         @FormParam("recipient") final String recipient,
         @FormParam("sender") final String sender,
         @FormParam("from") final String from,
-        @FormParam("subject") final String subject
+        @FormParam("subject") final String subject,
+        @FormParam("body-plain") final String bodyPlain
     ) {
         log.infov(
-            "Message received :: recipient={0}, sender={1}, from={2}, subject={3}",
+            "Message received :: recipient=\"{0}\", sender=\"{1}\", from=\"{2}\", subject=\"{3}\"",
             recipient, sender, from, subject);
+        log.infov("{0}", bodyPlain);
 
         // filterGroup.doFilter(Message.builder().build());
 
