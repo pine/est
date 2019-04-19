48f3bb962b53a5aa8d90b3bf98129492c4f7e491
Pine Mizune <pinemz@gmail.com>
2019-04-19 23:03:22 +0900

    fix: fix compile err

--- app/src/main/java/moe/pine/est/models/MessageRequest.java
+++ app/src/main/java/moe/pine/est/models/MessageRequest.java
@@ -1,11 +1,13 @@
 package moe.pine.est.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
 @NoArgsConstructor
 public class MessageRequest {
     private String recipient;
