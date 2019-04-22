bd3bee16f3a82b127dcb98d0c281aded88fabeb4
Pine Mizune <pinemz@gmail.com>
2019-04-22 14:06:53 +0900

    fix: add processors

--- lib/processor/src/main/java/moe/pine/est/processor/CompositeProcessor.java
+++ lib/processor/src/main/java/moe/pine/est/processor/CompositeProcessor.java
@@ -1,6 +1,7 @@
 package moe.pine.est.processor;
 
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 import moe.pine.est.email.models.EmailMessage;
 import org.apache.commons.collections4.CollectionUtils;
 
@@ -13,6 +14,7 @@ import java.util.stream.Stream;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 @RequiredArgsConstructor
+@ToString
 public class CompositeProcessor implements Processor {
     private final List<Processor> processors;
 
@@ -20,7 +22,7 @@ public class CompositeProcessor implements Processor {
     @Nonnull
     @Override
     public List<NotifyRequest> execute(
-        @Nonnull final EmailMessage message
+            @Nonnull final EmailMessage message
     ) {
         checkNotNull(message);
 
@@ -29,13 +31,13 @@ public class CompositeProcessor implements Processor {
         }
 
         return processors.stream()
-            .flatMap(processor -> {
-                final var notifyRequests = processor.execute(message);
-                if (CollectionUtils.isEmpty(notifyRequests)) {
-                    return Stream.empty();
-                }
-                return notifyRequests.stream();
-            })
-            .collect(Collectors.toUnmodifiableList());
+                .flatMap(processor -> {
+                    final var notifyRequests = processor.execute(message);
+                    if (CollectionUtils.isEmpty(notifyRequests)) {
+                        return Stream.empty();
+                    }
+                    return notifyRequests.stream();
+                })
+                .collect(Collectors.toUnmodifiableList());
     }
 }
