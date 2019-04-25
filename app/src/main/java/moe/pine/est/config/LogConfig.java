fc88d8565b187890941d97ac9cb594c0a4e80f55
Pine Mizune <pinemz@gmail.com>
2019-04-25 16:59:31 +0900

    feat: use mustache

--- app/src/main/java/moe/pine/est/config/LogConfig.java
+++ app/src/main/java/moe/pine/est/config/LogConfig.java
@@ -1,7 +1,12 @@
 package moe.pine.est.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class LogConfig {
+    @Bean
+    public String messageLogKeyFormat() {
+        return "message:{{dt}}{{#hash}}:{{hash}}{{/hash}}";
+    }
 }
--- lib/log/src/main/java/moe/pine/est/log/MessageLogRepository.java
+++ lib/log/src/main/java/moe/pine/est/log/MessageLogRepository.java
@@ -2,14 +2,20 @@ package moe.pine.est.log;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableMap;
 import lombok.extern.slf4j.Slf4j;
 import moe.pine.est.log.models.MessageLog;
 import moe.pine.est.murmur.Murmur3;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.Nonnull;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.time.Clock;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -17,30 +23,76 @@ import java.time.format.DateTimeFormatter;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 @Repository
-@RequiredArgsConstructor
 @Slf4j
 public class MessageLogRepository {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("YYYYMMdd");
+    private static final String DT_KEY = "dt";
+    private static final String HASH_KEY = "hash";
 
     private final RedisTemplate<String, String> redisTemplate;
     private final ObjectMapper objectMapper;
     private final Murmur3 murmur3;
     private final Clock clock;
+    private final Mustache keyFormat;
+
+    public MessageLogRepository(
+            final RedisTemplate<String, String> redisTemplate,
+            final ObjectMapper objectMapper,
+            final MustacheFactory mustacheFactory,
+            final Murmur3 murmur3,
+            final Clock clock,
+            @Qualifier("messageLogKeyFormat") final String keyFormat
+    ) {
+        this.redisTemplate = checkNotNull(redisTemplate);
+        this.objectMapper = checkNotNull(objectMapper);
+        this.murmur3 = checkNotNull(murmur3);
+        this.clock = checkNotNull(clock);
+        this.keyFormat = mustacheFactory.compile(new StringReader(keyFormat), "");
+    }
 
     @SuppressWarnings("ResultOfMethodCallIgnored")
-    public String add(@Nonnull final MessageLog messageLog)
+    public void add(@Nonnull final MessageLog messageLog)
             throws JsonProcessingException {
         checkNotNull(messageLog);
 
-        final String value = objectMapper.writeValueAsString(messageLog);
-        final String id = murmur3.hash128(value);
-        final String dt = LocalDate.now(clock).format(FORMATTER);
-        final String key = String.format("msg:%s:%s", dt, id);
+        final LocalDate dt = LocalDate.now(clock);
+        final String item = objectMapper.writeValueAsString(messageLog);
+        final String hash = murmur3.hash128(item);
+        final String itemsKey = computeItemsKey(dt);
+        final String itemKey = computeItemKey(dt, hash);
 
-        log.debug("key={}, value={}", key, value);
+        log.debug("items-key={}, item-key={}, value={}", itemsKey, itemKey, item);
 
         // redisTemplate.opsForValue().set(key, value);
+    }
 
-        return id;
+    @SuppressWarnings("WeakerAccess")
+    @VisibleForTesting
+    String computeItemKey(
+            final LocalDate dt,
+            final String hash
+    ) {
+        final var writer = new StringWriter();
+        final var scopes =
+                ImmutableMap.of(
+                        DT_KEY, dt.format(FORMATTER),
+                        HASH_KEY, hash
+                );
+        keyFormat.execute(writer, scopes);
+
+        return writer.toString();
+    }
+
+    private String computeItemsKey(
+            final LocalDate dt
+    ) {
+        final var writer = new StringWriter();
+        final var scopes =
+                ImmutableMap.of(
+                        DT_KEY, dt.format(FORMATTER)
+                );
+        keyFormat.execute(writer, scopes);
+
+        return writer.toString();
     }
 }
