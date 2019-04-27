6bbe7be29bf34a9eaf1639c085aa9fd4c1981f4e
Pine Mizune <pinemz@gmail.com>
2019-04-27 17:51:12 +0900

    fix: :innocent:

--- lib/log/src/main/java/moe/pine/est/log/models/MessageLog.java
+++ lib/log/src/main/java/moe/pine/est/log/models/MessageLog.java
@@ -1,5 +1,6 @@
 package moe.pine.est.log.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 @Data
 @Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class MessageLog {
     private String recipient;
     private String sender;
