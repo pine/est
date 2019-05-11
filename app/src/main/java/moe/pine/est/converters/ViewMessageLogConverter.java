6bddb85d0cfe65f33293848f1b66869992b1809d
Pine Mizune <pinemz@gmail.com>
2019-05-12 00:12:11 +0900

    fix: use defaultString

--- app/src/main/java/moe/pine/est/converters/ViewMessageLogConverter.java
+++ app/src/main/java/moe/pine/est/converters/ViewMessageLogConverter.java
@@ -2,6 +2,7 @@ package moe.pine.est.converters;
 
 import moe.pine.est.log.models.MessageLog;
 import moe.pine.est.models.ViewMessageLog;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Nonnull;
@@ -21,17 +22,17 @@ public class ViewMessageLogConverter {
         final var timestamp = LocalDateTime.ofInstant(instant, ZONE_OFFSET);
 
         return ViewMessageLog.builder()
-            .recipient(messageLog.getRecipient())
-            .sender(messageLog.getSender())
-            .from(messageLog.getFrom())
-            .subject(messageLog.getSubject())
-            .bodyPlain(messageLog.getBodyPlain())
-            .strippedText(messageLog.getStrippedText())
-            .strippedSignature(messageLog.getStrippedSignature())
-            .bodyHtml(messageLog.getBodyHtml())
+            .recipient(StringUtils.defaultString(messageLog.getRecipient()))
+            .sender(StringUtils.defaultString(messageLog.getSender()))
+            .from(StringUtils.defaultString(messageLog.getFrom()))
+            .subject(StringUtils.defaultString(messageLog.getSubject()))
+            .bodyPlain(StringUtils.defaultString(messageLog.getBodyPlain()))
+            .strippedText(StringUtils.defaultString(messageLog.getStrippedText()))
+            .strippedSignature(StringUtils.defaultString(messageLog.getStrippedSignature()))
+            .bodyHtml(StringUtils.defaultString(messageLog.getBodyHtml()))
             .timestamp(timestamp)
-            .token(messageLog.getToken())
-            .signature(messageLog.getSignature())
+            .token(StringUtils.defaultString(messageLog.getToken()))
+            .signature(StringUtils.defaultString(messageLog.getSignature()))
             .build();
     }
 }
